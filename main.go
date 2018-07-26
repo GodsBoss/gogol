9a0d097d0b204ea590551021cd74e5826b648d9b
Timo Reitz <godsboss@gmx.de>
2018-07-26 22:14:50 +0200

    Add function for filling a game with random values.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package gogol
 
 import (
+	"math"
 	"math/rand"
 	"time"
 )
@@ -10,5 +11,28 @@ func viveRandomFields(topology Topology, game *Game, count int) {
 	all := topology.All()
 	for i := 0; i < count; i++ {
 		game.Override(all[random.Intn(len(all))], Alive)
+	}
+}
+
+// FillWithRandomFields fills a game with cells randomly set to alive or dead with
+// a certain distribution.
+func FillWithRandomFields(game *Game, aliveShare float64) {
+	aliveShare = math.Min(1, math.Max(0, aliveShare))
+	origAll := game.topology.All()
+	all := make(FieldIDs, len(origAll))
+	copy(all, origAll)
+	random := rand.New(rand.NewSource(time.Now().Unix()))
+	random.Shuffle(
+		len(all),
+		func(i, j int) {
+			all[i], all[j] = all[j], all[i]
+		},
+	)
+	pivot := int(aliveShare * float64(len(all)))
+	for i := 0; i < pivot; i++ {
+		game.Override(all[i], Alive)
+	}
+	for i := pivot; i < len(all); i++ {
+		game.Override(all[i], Dead)
 	}
 }
